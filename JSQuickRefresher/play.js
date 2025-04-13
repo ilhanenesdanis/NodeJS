d7b4c326b3e34068e12d94e84223d4c5a2f7ff33
İlhan Enes Daniş <danisenes3@gmail.com>
2025-04-13 18:22:52 +0300

    arrow function

--- JSQuickRefresher/play.js
+++ JSQuickRefresher/play.js
@@ -12,4 +12,34 @@ function summarizeUser(userName, userAge, userHasHobby) {
     userHasHobby
   );
 }
-console.log(summarizeUser(name,age,hasHobbies))
+//console.log(summarizeUser(name,age,hasHobbies))
+
+//let = mutable
+//const =immutable
+let userName = "ilhan";
+let userAge = 24;
+const hasHobbiesUser = true;
+
+const newSummurizeUser = (userName, userAge, userHasHobbies) => {
+  return (
+    "Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the user has hobbies: " +
+    userHasHobbies
+  );
+};
+
+console.log(newSummurizeUser(userName, userAge, hasHobbiesUser));
+
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(125, 1145));
+
+const addOne = (a) => a + 1;
+
+console.log(addOne(5))
+
