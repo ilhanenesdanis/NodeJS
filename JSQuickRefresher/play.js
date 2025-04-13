b4ddcf8b5a5fca30beac0c62bfe52d4341ec0386
İlhan Enes Daniş <danisenes3@gmail.com>
2025-04-13 18:02:11 +0300

    first commit

---
+++ JSQuickRefresher/play.js
@@ -0,0 +1,15 @@
+var name = "Max";
+var age = 24;
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHobby) {
+  return (
+    "Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the user has hobbies: " +
+    userHasHobby
+  );
+}
+console.log(summarizeUser(name,age,hasHobbies))
